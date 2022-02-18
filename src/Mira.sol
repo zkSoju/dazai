ff9c9b04816bc1f1a9efa3f0c5f11cf4d85d9852
Alan Le <underrated@gmail.com>
2022-02-18 00:43:57 -0800

    🧪 Signed mint tests

--- src/Mira.sol
+++ src/Mira.sol
@@ -10,162 +10,183 @@ import "@openzeppelin/utils/cryptography/ECDSA.sol";
 
 /// @author Enigma Labs <gm@enigmalabs.gg>
 contract Mira is Ownable, ReentrancyGuard, ERC721A {
-  using ECDSA for bytes32;
-  using MerkleProof for bytes32[];
+    using ECDSA for bytes32;
+    using MerkleProof for bytes32[];
 
-  error NotAuthorized();
-  error NotAllowed();
-  error AlreadyMinted();
-  error InsufficientValue();
-  error ExceededMaxMints();
-  error UnsafeRecipient();
-  error InvalidAction();
+    error NotAuthorized();
+    error NotAllowed();
+    error AlreadyMinted();
+    error InsufficientValue();
+    error ExceededMaxMints();
+    error UnsafeRecipient();
+    error InvalidAction();
 
-  //   struct SaleConfig {
-  //     uint32 auctionSaleStartTime;
-  //     uint32 publicSaleStartTime;
-  //     uint64 allowlistPrice;
-  //     uint64 publicPrice;
-  //     uint32 publicSaleKey;
-  //   }
+    //   struct SaleConfig {
+    //     uint32 auctionSaleStartTime;
+    //     uint32 publicSaleStartTime;
+    //     uint64 allowlistPrice;
+    //     uint64 publicPrice;
+    //     uint32 publicSaleKey;
+    //   }
 
-  //   SaleConfig public saleConfig;
+    //   SaleConfig public saleConfig;
 
-  uint256 public allowlistMaxMint;
-  uint256 public publicMaxMint;
-  uint256 public collectionSize;
-  uint256 public price;
-  bytes32 public merkleRoot;
+    uint256 public allowlistMaxMint;
+    uint256 public publicMaxMint;
+    uint256 public collectionSize;
+    uint256 public price;
+    bytes32 public merkleRoot;
 
-  address public signerAddress;
+    address public signerAddress = 0x165A20A378DEb66e5eF349cbeA72302838F2B0AF;
 
-  string private _baseTokenURI;
+    string private _baseTokenURI;
 
-  //   uint256 public constant AUCTION_START_PRICE = 1 ether;
-  //   uint256 public constant AUCTION_END_PRICE = 0.15 ether;
-  //   uint256 public constant AUCTION_PRICE_CURVE_LENGTH = 340 minutes;
-  //   uint256 public constant AUCTION_DROP_INTERVAL = 20 minutes;
-  //   uint256 public constant AUCTION_DROP_PER_STEP =
-  //     (AUCTION_START_PRICE - AUCTION_END_PRICE) /
-  //       (AUCTION_PRICE_CURVE_LENGTH / AUCTION_DROP_INTERVAL);
+    uint256 public constant AUCTION_START_PRICE = 1 ether;
+    uint256 public constant AUCTION_END_PRICE = 0.15 ether;
+    uint256 public constant AUCTION_PRICE_CURVE_LENGTH = 340 minutes;
+    uint256 public constant AUCTION_DROP_INTERVAL = 20 minutes;
+    uint256 public constant AUCTION_DROP_PER_STEP =
+        (AUCTION_START_PRICE - AUCTION_END_PRICE) /
+            (AUCTION_PRICE_CURVE_LENGTH / AUCTION_DROP_INTERVAL);
 
-  mapping(address => uint256) public allowlistClaimed;
-  mapping(address => uint256) public publicClaimed;
+    mapping(address => uint256) public allowlistClaimed;
+    mapping(address => uint256) public publicClaimed;
 
-  constructor(
-    string memory _name,
-    string memory _symbol,
-    uint256 _collectionSize,
-    bytes32 _merkleRoot,
-    uint256 _allowlistMaxMint,
-    uint256 _publicMaxMint,
-    uint256 _price
-  ) ERC721A(_name, _symbol) {
-    collectionSize = _collectionSize;
-    merkleRoot = _merkleRoot;
-    allowlistMaxMint = _allowlistMaxMint;
-    publicMaxMint = _publicMaxMint;
-    price = _price;
-  }
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _collectionSize,
+        bytes32 _merkleRoot,
+        uint256 _allowlistMaxMint,
+        uint256 _publicMaxMint,
+        uint256 _price
+    ) ERC721A(_name, _symbol) {
+        collectionSize = _collectionSize;
+        merkleRoot = _merkleRoot;
+        allowlistMaxMint = _allowlistMaxMint;
+        publicMaxMint = _publicMaxMint;
+        price = _price;
+    }
 
-  /// @dev Verify sender is a user and not a contract
-  modifier callerIsUser() {
-    if (tx.origin != msg.sender) revert NotAuthorized();
-    _;
-  }
+    /// @dev Verify sender is a user and not a contract
+    modifier callerIsUser() {
+        if (tx.origin != msg.sender) revert NotAuthorized();
+        _;
+    }
 
-  /// @dev Verify account address exists in Merkle Tree
-  function verifyAllowlist(bytes32[] calldata proof, address account)
-    public
-    view
-    returns (bool)
-  {
-    bytes32 leaf = keccak256(abi.encodePacked(account));
-    return proof.verify(merkleRoot, leaf);
-  }
+    /// @dev Verify account address exists in Merkle Tree
+    function verifyAllowlist(bytes32[] calldata proof, address account)
+        public
+        view
+        returns (bool)
+    {
+        bytes32 leaf = keccak256(abi.encodePacked(account));
+        return proof.verify(merkleRoot, leaf);
+    }
 
-  /// @dev Set a new merkle root for seeding new data.
-  function setMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
-    merkleRoot = _merkleRoot;
-  }
+    /// @dev Set a new merkle root for seeding new data.
+    function setMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
+        merkleRoot = _merkleRoot;
+    }
 
-  /// @dev Mint reserved quantity for marketing and development.
-  function mintReserved(uint256 quantity) external onlyOwner {
-    _safeMint(msg.sender, quantity);
-  }
+    function _senderHash() internal view returns (bytes32) {
+        return
+            keccak256(
+                abi.encodePacked(
+                    "\x19Ethereum Signed Message:\n32",
+                    keccak256(abi.encodePacked(msg.sender))
+                )
+            );
+    }
 
-  /// @dev Mint reserved quantity for marketing and development.
-  function mintAllowlist(bytes32[] calldata _merkleProof, uint256 quantity)
-    external
-    payable
-    callerIsUser
-  {
-    if (!(verifyAllowlist(_merkleProof, msg.sender))) revert NotAllowed();
-    if (allowlistClaimed[msg.sender] + quantity > allowlistMaxMint)
-      revert AlreadyMinted();
-    if (msg.value < quantity * price) revert InsufficientValue();
-    if (totalSupply() + quantity > collectionSize) revert InvalidAction();
+    function verify(bytes32 _hash, bytes memory _signature)
+        public
+        view
+        returns (bool)
+    {
+        if (_senderHash() != _hash) {
+            return false;
+        }
+        return _hash.recover(_signature) == signerAddress;
+    }
 
-    allowlistClaimed[msg.sender] += quantity;
-    _safeMint(msg.sender, quantity);
-  }
+    /// @dev Mint reserved quantity for marketing and development.
+    function mintReserved(uint256 _quantity) external onlyOwner {
+        _safeMint(msg.sender, _quantity);
+    }
 
-  //   /// @dev Mint for public sale, limited to `publicMaxMints`.
-  //   function mintPublicSale(uint256 quantity, bytes calldata hash)
-  //     external
-  //     payable
-  //     callerIsUser
-  //   {
-  //     // SaleConfig memory config = saleConfig;
-  //     // uint256 publicSaleKey = uint256(config.publicSaleKey);
-  //     // uint256 publicPrice = uint256(config.publicPrice);
-  //     // uint256 publicSaleStartTime = uint256(config.publicSaleStartTime);
-  //     if (!(_verify(_hash(msg.sender), hash))) revert NotAuthorized();
-  //     if (publicClaimed[msg.sender] + quantity > publicMaxMint)
-  //       revert AlreadyMinted();
-  //     if (msg.value < quantity * price) revert InsufficientValue();
-  //     if (totalSupply() + quantity > collectionSize) revert InvalidAction();
+    /// @dev Mint reserved quantity for marketing and development.
+    function mintAllowlist(uint256 _quantity, bytes32[] calldata _merkleProof)
+        external
+        payable
+        callerIsUser
+    {
+        if (!(verifyAllowlist(_merkleProof, msg.sender))) revert NotAllowed();
+        if (allowlistClaimed[msg.sender] + _quantity > allowlistMaxMint)
+            revert AlreadyMinted();
+        if (msg.value < _quantity * price) revert InsufficientValue();
+        if (totalSupply() + _quantity > collectionSize) revert InvalidAction();
 
-  //     publicClaimed[msg.sender] += quantity;
+        allowlistClaimed[msg.sender] += _quantity;
+        _safeMint(msg.sender, _quantity);
+    }
 
-  //     _safeMint(msg.sender, quantity);
-  //   }
+    /// @dev Mint for public sale, limited to `publicMaxMints`.
+    function mintPublicSale(
+        uint256 _quantity,
+        bytes32 _hash,
+        bytes memory _signature
+    ) external payable callerIsUser {
+        // SaleConfig memory config = saleConfig;
+        // uint256 publicSaleKey = uint256(config.publicSaleKey);
+        // uint256 publicPrice = uint256(config.publicPrice);
+        // uint256 publicSaleStartTime = uint256(config.publicSaleStartTime);
+        if (!(verify(_hash, _signature))) revert NotAuthorized();
+        if (publicClaimed[msg.sender] + _quantity > publicMaxMint)
+            revert AlreadyMinted();
+        if (msg.value < _quantity * price) revert InsufficientValue();
+        if (totalSupply() + _quantity > collectionSize) revert InvalidAction();
 
-  //   function getAuctionPrice(uint256 saleStartTime)
-  //     public
-  //     view
-  //     returns (uint256)
-  //   {
-  //     if (block.timestamp < saleStartTime) {
-  //       return AUCTION_START_PRICE;
-  //     }
-  //     if (block.timestamp - saleStartTime >= AUCTION_PRICE_CURVE_LENGTH) {
-  //       return AUCTION_END_PRICE;
-  //     } else {
-  //       uint256 steps = (block.timestamp - saleStartTime) / AUCTION_DROP_INTERVAL;
-  //       return AUCTION_START_PRICE - (steps * AUCTION_DROP_PER_STEP);
-  //     }
-  //   }
+        publicClaimed[msg.sender] += _quantity;
 
-  //   function setAuctionStart(uint32 timestamp) external onlyOwner {
-  //     saleConfig.auctionSaleStartTime = timestamp;
-  //   }
+        _safeMint(msg.sender, _quantity);
+    }
 
-  //   function setPublicSaleKey(uint32 key) external onlyOwner {
-  //     saleConfig.publicSaleKey = key;
-  //   }
+    //   function getAuctionPrice(uint256 saleStartTime)
+    //     public
+    //     view
+    //     returns (uint256)
+    //   {
+    //     if (block.timestamp < saleStartTime) {
+    //       return AUCTION_START_PRICE;
+    //     }
+    //     if (block.timestamp - saleStartTime >= AUCTION_PRICE_CURVE_LENGTH) {
+    //       return AUCTION_END_PRICE;
+    //     } else {
+    //       uint256 steps = (block.timestamp - saleStartTime) / AUCTION_DROP_INTERVAL;
+    //       return AUCTION_START_PRICE - (steps * AUCTION_DROP_PER_STEP);
+    //     }
+    //   }
 
-  /// @dev Transfer contract funds to owner address.
-  function withdrawFunds() external onlyOwner nonReentrant {
-    (bool success, ) = msg.sender.call{ value: address(this).balance }("");
-    if (!success) revert InvalidAction();
-  }
+    //   function setAuctionStart(uint32 timestamp) external onlyOwner {
+    //     saleConfig.auctionSaleStartTime = timestamp;
+    //   }
 
-  function _baseURI() internal view virtual override returns (string memory) {
-    return _baseTokenURI;
-  }
+    //   function setPublicSaleKey(uint32 key) external onlyOwner {
+    //     saleConfig.publicSaleKey = key;
+    //   }
 
-  function setBaseURI(string calldata baseURI) external onlyOwner {
-    _baseTokenURI = baseURI;
-  }
+    /// @dev Transfer contract funds to owner address.
+    function withdrawFunds() external onlyOwner nonReentrant {
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        if (!success) revert InvalidAction();
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function setBaseURI(string calldata baseURI) external onlyOwner {
+        _baseTokenURI = baseURI;
+    }
 }
--- src/test/Mira.t.sol
+++ src/test/Mira.t.sol
@@ -77,7 +77,7 @@ contract ContractTest is DSTestPlus {
         // Expect revert minted from smart contract not user wallet
         hoax(address(1337));
         vm.expectRevert(abi.encodePacked(bytes4(keccak256("NotAuthorized()"))));
-        mira.mintAllowlist(proof, 3);
+        mira.mintAllowlist(uint256(3), proof);
 
         // Set tx.origin and msg.sender for user address testing
         startHoax(address(1337), address(1337));
@@ -86,48 +86,46 @@ contract ContractTest is DSTestPlus {
         vm.expectRevert(
             abi.encodePacked(bytes4(keccak256("InsufficientValue()")))
         );
-        mira.mintAllowlist(proof, uint256(3));
+        mira.mintAllowlist(uint256(3), proof);
 
         // Expect revert not enough ether provided
         vm.expectRevert(
             abi.encodePacked(bytes4(keccak256("InsufficientValue()")))
         );
-        mira.mintAllowlist{value: 1}(proof, uint256(3));
+        mira.mintAllowlist{value: 1}(uint256(3), proof);
 
         // Expect revert no ether sent
         vm.expectRevert(
             abi.encodePacked(bytes4(keccak256("InsufficientValue()")))
         );
-        mira.mintAllowlist(proof, uint256(3));
+        mira.mintAllowlist(uint256(3), proof);
 
         // Expect revert bad proof submitted
         vm.expectRevert(abi.encodePacked(bytes4(keccak256("NotAllowed()"))));
-        mira.mintAllowlist(badProof, uint256(3));
+        mira.mintAllowlist(uint256(3), badProof);
 
         // Successfully mint tokens
-        mira.mintAllowlist{value: 3}(proof, uint256(3));
+        mira.mintAllowlist{value: 3}(uint256(3), proof);
 
         // Expect revert already minted max mints
         vm.expectRevert(abi.encodePacked(bytes4(keccak256("AlreadyMinted()"))));
-        mira.mintAllowlist(proof, uint256(3));
+        mira.mintAllowlist(uint256(3), proof);
         vm.stopPrank();
 
         // Expect revert invalid proof + not on allowlist
         hoax(address(0xBEEF), address(0xBEEF));
         vm.expectRevert(abi.encodePacked(bytes4(keccak256("NotAllowed()"))));
-        mira.mintAllowlist(proof, uint256(3));
+        mira.mintAllowlist(uint256(3), proof);
 
         // Validate mints
         assert(mira.totalSupply() == uint256(3));
         assert(mira.allowlistClaimed(address(1337)) == uint256(3));
     }
 
-    function testRestrictedMint() public {}
-
     function testWithdraw() public {
         // Non-owner shouldn't be able to withdraw
         hoax(address(0xBEEF), address(0xBEEF));
-        vm.expectRevert("Ownable: caller is not the owner");
+        vm.expectRevert(abi.encodePacked("Ownable: caller is not the owner"));
         mira.withdrawFunds();
 
         // Parse merkle proof
@@ -141,13 +139,105 @@ contract ContractTest is DSTestPlus {
 
         // Foward successful mint funds to contract balance
         hoax(address(1337), address(1337));
-        mira.mintAllowlist{value: 2}(proof, 2);
+        mira.mintAllowlist{value: 2}(2, proof);
 
         // Successful withdraw as owner
         uint256 beforeBalance = address(this).balance;
         mira.withdrawFunds();
         uint256 afterBalance = address(this).balance;
         assert(afterBalance - beforeBalance == uint256(2));
+    }
+
+    function testSignature() public {
+        startHoax(
+            address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),
+            address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4)
+        );
+
+        // Ensure successful recovery of signer address
+        bytes
+            memory signature = hex"6c69643ab47e09b82e4a2bb692e412367ded1835d2ed7f569f7ba33a31a1559206237ec92e885fbe6bf54001b4ad73a08e44144ef0dde169e10734550ca5bde41c";
+
+        bytes
+            memory fakeSignature = hex"8c69643ab47e09b82e4a2bb692e412367ded1835d2ed7f569f7ba33a31a1559206237ec92e885fbe6bf54001b4ad73a08e44144ef0dde169e10734550ca5bde41c";
+
+        bool isVerified = mira.verify(
+            0xcd9ed0433174d173b609ed57aa4b81fb9b9dc8b800fb0b7743d1d703bacf1b24,
+            signature
+        );
+        assertTrue(isVerified);
+
+        // Assert false for incorrect RPC message hash of msg.sender
+        bool isFakeHash = mira.verify(
+            0xcd9ed0433174d173b609ed57aa4b81fb9b9dc8b800fb0b7743d1d703bacf1b88,
+            signature
+        );
+        assertTrue(!isFakeHash);
+
+        // Expect revert for incorrect signature/signer
+        vm.expectRevert(abi.encodePacked("ECDSA: invalid signature"));
+        bool isFakeSigner = mira.verify(
+            0xcd9ed0433174d173b609ed57aa4b81fb9b9dc8b800fb0b7743d1d703bacf1b24,
+            fakeSignature
+        );
+        vm.stopPrank();
+    }
+
+    function testSignedMint() public {
+        startHoax(
+            address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),
+            address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4)
+        );
+
+        bytes
+            memory signature = hex"6c69643ab47e09b82e4a2bb692e412367ded1835d2ed7f569f7ba33a31a1559206237ec92e885fbe6bf54001b4ad73a08e44144ef0dde169e10734550ca5bde41c";
+        bytes
+            memory fakeSignature = hex"8c69643ab47e09b82e4a2bb692e412367ded1835d2ed7f569f7ba33a31a1559206237ec92e885fbe6bf54001b4ad73a08e44144ef0dde169e10734550ca5bde41c";
+
+        bytes32 incorrectSenderHash = keccak256(
+            abi.encodePacked(
+                address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4)
+            )
+        );
+
+        bytes32 senderHash = keccak256(
+            abi.encodePacked(
+                "\x19Ethereum Signed Message:\n32",
+                keccak256(
+                    abi.encodePacked(
+                        address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4)
+                    )
+                )
+            )
+        );
+
+        // Expect revert no ether sent
+        vm.expectRevert(
+            abi.encodePacked(bytes4(keccak256("InsufficientValue()")))
+        );
+        mira.mintPublicSale{value: 0}(uint256(1), senderHash, signature);
+
+        // Expect revert fake credentials
+        vm.expectRevert(abi.encodePacked(bytes4(keccak256("NotAuthorized()"))));
+        mira.mintPublicSale{value: 1}(
+            uint256(1),
+            incorrectSenderHash,
+            signature
+        );
+
+        vm.expectRevert(abi.encodePacked(bytes4(keccak256("NotAuthorized()"))));
+        mira.mintPublicSale{value: 1}(
+            uint256(1),
+            incorrectSenderHash,
+            fakeSignature
+        );
+
+        vm.expectRevert(abi.encodePacked("ECDSA: invalid signature"));
+        mira.mintPublicSale{value: 1}(uint256(1), senderHash, fakeSignature);
+
+        // Ensure successful mint provided correct credentials
+        mira.mintPublicSale{value: 1}(uint256(1), senderHash, signature);
+        vm.stopPrank();
     }
 
     fallback() external payable {}
